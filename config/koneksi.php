6d982892a0e6888982886c6a00a2dec1e4e5807c
Ridwan0126 <muhamadridwan6792@gmail.com>
2024-05-26 21:13:22 +0700

    commit update bug dan memperbaiki error

---
+++ config/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "db_arsipwarga";
+
+$db = mysqli_connect($host, $user, $pass, $database) or die("gagal koneksi ke database");
